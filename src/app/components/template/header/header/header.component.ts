2f7d7998a6c3854a5e1861ee3756d6b10b2a584e
Joao Victor Longo <joaovictorlongoo@gmail.com>
2022-03-07 23:32:59 -0300

    fix http calls and error messages

--- src/app/components/template/header/header/header.component.ts
+++ src/app/components/template/header/header/header.component.ts
@@ -1,26 +1,25 @@
-import { Component, Injectable, OnInit } from '@angular/core';
-import { SearchService } from 'src/app/services/search/search.service';
+import { Component, Injectable, OnInit } from "@angular/core";
+import { SearchService } from "src/app/services/search/search.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private searchService: SearchService) {}
 
-  constructor(private searchService: SearchService) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   handleSearch(query: string, page: number = 1) {
     this.searchService.searchQueryObserver.emit(query);
-    this.searchService.find(query, page);
+    this.searchService
+      .find(query, page)
+      .subscribe((response) =>
+        this.searchService.dataChangeObserver.emit(response)
+      );
   }
-
-
 }
