884eb4ba454f1b9d3080e56a4ab17c54022072a8
Joao Victor Longo <joaovictorlongoo@gmail.com>
2022-03-07 23:32:59 -0300

    fix http calls and error messages

--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -1,47 +1,51 @@
-import { EventEmitter, Injectable, Output } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { GithubApi } from 'src/app/models/Repository.model';
-import { EMPTY, Observable } from 'rxjs';
+import { EventEmitter, Injectable, Output } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { GithubApi } from "src/app/models/Repository.model";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class SearchService {
+  baseURL = "https://api.github.com";
 
-  baseURL= 'https://api.github.com';
-
-  @Output() searchQueryObserver : EventEmitter<string> = new EventEmitter();
+  @Output() searchQueryObserver: EventEmitter<string> = new EventEmitter();
   @Output() dataChangeObserver: EventEmitter<GithubApi> = new EventEmitter();
 
-  constructor(private snackBar: MatSnackBar, private _httpClient: HttpClient)  { }
+  constructor(private snackBar: MatSnackBar, private _httpClient: HttpClient) {}
 
-  find(search: string, page: number = 1) {
-    this._httpClient.get<GithubApi>(`${this.baseURL}/search/repositories?q=${search}&&page=${page}`)
-      .subscribe(
-        response => this.dataChangeObserver.emit(response),
-        error => this.errorHandler(error)
-      );
-  }
-
-  errorHandler(e: HttpErrorResponse): Observable<any> {
-    if (e.status === 422) {
-      this.showMessage('Digite algum termo para pesquisa');
-    } else {
-      this.showMessage('Ocorreu um erro ao pesquisar')
-    }    
-    return EMPTY;
-  }
-  
-  showMessage(msg: string, isError: boolean = true): void {
-    this.snackBar.open(msg, "X", {
-      duration: 3000,
-      horizontalPosition: "right",
-      verticalPosition: "top",
+  showMessage(msg: string, isError: boolean = true) {
+    this.snackBar.open(msg, "x", {
+      duration: 5000,
+      horizontalPosition: "center",
+      verticalPosition: "bottom",
       panelClass: isError ? ["msg-error"] : ["msg-success"],
     });
   }
+
+  find(search: string, page: number = 1): Observable<GithubApi> {
+    return this._httpClient
+      .get<GithubApi>(
+        `${this.baseURL}/search/repositories?q=${search}&&page=${page}`
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError = (error: HttpErrorResponse) => {
+    if (error.status === 0) {
+      console.error("An error occurred:", error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+          `body was: ${error.error.message}`
+      );
+    }
+    return throwError(() =>
+      this.showMessage(
+        "Ocorreu um erro ao realizar a pesquisa, verifique o termo digitado, conexão com a internet e tente novamente."
+      )
+    );
+  };
 }
-
-
-
